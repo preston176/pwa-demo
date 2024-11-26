b5220bc6755fdca70dfe99ce5096d9d1c75a2704
preston176 <prestonnyamweya@gmail.com>
2024-11-26 18:58:54 +0300

    Add Parcel dependencies, update manifest icons, and enhance service worker functionality

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ installButton.addEventListener('click', async () => {
 });
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js').then((registration) => {
+    navigator.serviceWorker.register(new URL('service-worker.js', import.meta.url)).then((registration) => {
         console.log('Service Worker registered with scope:', registration.scope);
     }).catch((error) => {
         console.log('Service Worker registration failed:', error);
